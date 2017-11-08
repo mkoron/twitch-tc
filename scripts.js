3cf8893145b6cbceb20b84aa02b0954e5645bcbb
Marko Koron <koron.marko@gmail.com>
2017-11-08 18:21:09 +0100

    Fetching data / created table

--- scripts.js
+++ scripts.js
@@ -1,1 +1,58 @@
-var twitch_client_id = tp1qev62ulhdjnr991y8c8mt1fdcud;
+
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","dummy_user_12789"];
+var no_avatar_image = 'https://dummyimage.com/100x100/fff/71738f.png&text=No+user+image';
+
+
+$(function(){
+    let rowNumber = 1;
+    let userName, avatar, game, url, userRow;
+
+    for(let i = 0; i < users.length; i++) {
+        $.ajax({
+            type: 'GET',
+            url: `https://wind-bow.gomix.me/twitch-api/channels/${users[i]}`,
+            dataType: 'jsonp',
+            success: function (data) {
+                userName = data.display_name || users[i];
+                avatar = data.logo || no_avatar_image;
+                game = data.game;
+                url = data.url
+                
+                if (data.error) {
+                    userRow = `<tr class="bg-danger">
+                    <th scope='row'>${rowNumber++}</th>
+                    <td colspan='4'>${data.message}</td>
+                   </tr>`
+                }
+                else {
+                    userRow = `<tr>
+                                <th scope='row'>${rowNumber++}</th>
+                                <td class='avatar'><img src="${avatar}"></td>
+                                <td class='username'><a href="${url}" target="blank">${userName}</a></td>
+                                <td class='status'>**</td>
+                                <td class='game'>${game}</td>
+                            </tr>`
+                    console.log(data.error);
+                }
+
+                $('#results').append(userRow);
+            },
+            error: function (err){
+               console.log("Something went wrong...");
+            }
+        });
+        $.ajax({
+            type: 'GET',
+            url: `https://wind-bow.gomix.me/twitch-api/streams/${users[i]}`,
+            dataType: 'jsonp',
+            success: function (data) {
+
+                console.log(data);
+            },
+            error: function (err){
+                console.log("Something went wrong...");
+            }
+        });
+    }
+})
+
