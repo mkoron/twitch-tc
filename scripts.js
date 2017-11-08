8487df3221c06ff8f28016ddfa5aa1ccc9871741
Marko Koron <koron.marko@gmail.com>
2017-11-08 21:13:25 +0100

    Fixed wrong displaying wrong status

--- scripts.js
+++ scripts.js
@@ -4,55 +4,64 @@ var no_avatar_image = 'https://dummyimage.com/100x100/fff/71738f.png&text=No+use
 
 
 $(function(){
+
     let rowNumber = 1;
-    let userName, avatar, game, url, userRow;
 
     for(let i = 0; i < users.length; i++) {
-        $.ajax({
-            type: 'GET',
-            url: `https://wind-bow.gomix.me/twitch-api/channels/${users[i]}`,
-            dataType: 'jsonp',
-            success: function (data) {
-                userName = data.display_name || users[i];
-                avatar = data.logo || no_avatar_image;
-                game = data.game;
-                url = data.url
-                
-                if (data.error) {
+        let getStreams = $.ajax({
+                type: 'GET',
+                url: `https://wind-bow.gomix.me/twitch-api/streams/${users[i]}`,
+                dataType: 'jsonp',
+                success: function (data) {
+                },
+                error: function (err){
+                    console.log("Something went wrong...");
+                }
+            });
+
+        let getChannels = $.ajax({
+                type: 'GET',
+                url: `https://wind-bow.gomix.me/twitch-api/channels/${users[i]}`,
+                dataType: 'jsonp',
+                success: function (data) {
+                },
+                error: function (err){
+                console.log("Something went wrong...");
+                }
+            });
+
+            $.when(getStreams, getChannels).then(function(streamsData, channelsData){
+         
+                let userRow, statusColor;
+
+                let streamingStatus = streamsData[0].stream;
+
+                let userName = channelsData[0].display_name || users[i];
+                let avatar = channelsData[0].logo || no_avatar_image;                
+                let game = channelsData[0].game || '-';
+                let url = channelsData[0].url
+
+                statusColor = streamingStatus ? 'green' : 'red';
+
+                if (channelsData[0].error) {
                     userRow = `<tr class="bg-danger">
                     <th scope='row'>${rowNumber++}</th>
-                    <td colspan='4'>${data.message}</td>
+                    <td colspan='4'>${channelsData[0].message}</td>
                    </tr>`
                 }
+
                 else {
                     userRow = `<tr>
                                 <th scope='row'>${rowNumber++}</th>
                                 <td class='avatar'><img src="${avatar}"></td>
                                 <td class='username'><a href="${url}" target="blank">${userName}</a></td>
-                                <td class='status'>**</td>
+                                <td class='status '><span class='${statusColor}'></span></td>
                                 <td class='game'>${game}</td>
                             </tr>`
-                    console.log(data.error);
                 }
 
-                $('#results').append(userRow);
-            },
-            error: function (err){
-               console.log("Something went wrong...");
-            }
-        });
-        $.ajax({
-            type: 'GET',
-            url: `https://wind-bow.gomix.me/twitch-api/streams/${users[i]}`,
-            dataType: 'jsonp',
-            success: function (data) {
-
-                console.log(data);
-            },
-            error: function (err){
-                console.log("Something went wrong...");
-            }
-        });
+                $('#results').append(userRow);   
+            });
     }
 })
 
